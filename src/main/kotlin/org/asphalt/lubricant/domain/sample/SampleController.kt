9e421f59d8ce63d205734f60e172b1f67e587c23
ryan.woo <sk16006@skcorp.com>
2024-07-18 18:32:52 +0900

    Add: swagger config, open-session-in-view to false

--- src/main/kotlin/org/asphalt/lubricant/domain/sample/SampleController.kt
+++ src/main/kotlin/org/asphalt/lubricant/domain/sample/SampleController.kt
@@ -2,9 +2,11 @@ package org.asphalt.lubricant.domain.sample
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/v1")
 class SampleController {
     @GetMapping("/hello")
     fun hello(): ResponseEntity<String> = ResponseEntity.ok("Hello, World!")
